f603df3f4e520ab124f1c6a7b9b13bd4b9c0a5ea
vwaghmode <vikas@nice.com>
2021-09-15 21:41:42 +0530

    update

--- vars/test.groovy
+++ vars/test.groovy
@@ -10,8 +10,14 @@ def getRepoURL(String url){
 }
 
 def checkoutRepo(String branchName){
-	checkout(scm: [$class             : 'GitSCM', 
+/*	checkout(scm: [$class             : 'GitSCM', 
 			   branches          : [[name: + 'branchName']], 
 			   userRemoteConfigs : [[url: + getRepoURL.repoURL ]]
+*/	script{
+	def codeCheckout = "checkout([$class: 'GitSCM', branches: [[name: '*/main']], userRemoteConfigs: [[credentialsId: 'vwaghmode19-github',          url: 'git@github.com:vwaghmode19/jenkins_shared_library.git']]])"
+	sh script: codeCheckout
+	}
+
+
 	])
 }
