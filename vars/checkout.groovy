05f4fc5126a27e23ce113b1f707a7bc90f86f35e
vwaghmode <vikas@nice.com>
2021-09-15 22:45:58 +0530

    update

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,13 +1,14 @@
+@NonCPS
 def checkout(String repo, String branch = "main"){
     stage("Checkout"){
-        checkout([
+        checkout scm: [
             $class: 'GitSCM', 
             branches: [[name: "${branch}"]], 
             userRemoteConfigs: [[
                 credentialsId: "env.GITHUB_CREDENTIALS_ID", 
                 url: "${repo}"
             ]]
-        ])
+        ]
     }
 }
 
