27565e51e37be72a09a73a67f09ebb662869e7a8
vwaghmode <vikas@nice.com>
2021-09-16 09:05:55 +0530

    update

---
+++ vars/code.groovy
@@ -0,0 +1,11 @@
+def CodeCheckout(){
+    stage('Checkout') {
+		script{
+		    checkout([$class           : 'GitSCM', 
+		         branches          : [[name: 'master']], 
+                 userRemoteConfigs : [[credentialsId: 'vwaghmode19-github', 
+                                    url: 'https://github.com/vwaghmode19/simple-java-maven-app.git']]
+            ])
+		}
+	}
+}
