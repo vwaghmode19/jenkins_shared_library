c1f1ab007def4592e6a08c47fd3f7cf3b5281213
vwaghmode <vikas@nice.com>
2021-09-16 09:33:47 +0530

    update

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,15 +1,12 @@
-@NonCPS
-def checkOut(){
-    def LinkedHashMap<String, String> lhm = new LinkedHashMap<String, String>();
-  
-        // Add mappings using put method
-        lhm.put("git@github.com:vwaghmode19/jenkins_shared_library.git", "main");
-		lhm.checkout([
-            $class: 'GitSCM', 
-            branches: [[name: "${branch}"]], 
-            userRemoteConfigs: [[
-                credentialsId: "${GITHUB_CREDENTIALS_ID}", 
-                url: "${repo}"
-            ]]
-        ])
+void CodeCheckout(String node, String repoURL, String branchName, String credentialsId){
+	node(node){
+		stage('Checkout') {
+			script{
+				checkout([$class           : 'GitSCM', 
+					branches          : [[name: branchName]], 
+					userRemoteConfigs : [[credentialsId: credentialsId, url: repoURL]]
+				])
+			}
+		}
+	}
 }
