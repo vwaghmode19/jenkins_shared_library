902ac75e5c444c1309bd12240e4d3771624dc97a
vwaghmode <vikas@nice.com>
2021-09-16 08:28:43 +0530

    update

---
+++ vars/z.groovy
@@ -0,0 +1,11 @@
+void prepareInfra(String infraNode, String repoURL, String repoBranch) {
+    node(infraNode) {
+        stage('Checkout infra') {
+            checkout changelog: true, poll: true, scm: [
+                    $class                           : 'GitSCM',
+                    branches                         : [[name: repoBranch]],
+                    doGenerateSubmoduleConfigurations: false,
+                    userRemoteConfigs                : [[credentialsId: 'vwaghmode19-github-account', url: repoURL]]]
+        }
+    }
+}
