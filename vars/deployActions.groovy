fa57a131db15aa8b43da3c7aba04c7463d2529c2
vwaghmode <vikas@nice.com>
2021-09-16 07:58:45 +0530

    update

---
+++ vars/deployActions.groovy
@@ -0,0 +1,9 @@
+void infraCheckout() {
+    checkout scm
+//    checkout changelog: true, poll: true, scm: [
+//            $class                           : 'GitSCM',
+//            extensions                       : [],
+//            branches                         : [[name: 'master']],
+//            doGenerateSubmoduleConfigurations: false,
+//            userRemoteConfigs                : [[credentialsId: 'jenkins_v10', url: 'https://tlvgit02.nice.com/scm/v10/devops-devci-common-infra.git']]]
+}
