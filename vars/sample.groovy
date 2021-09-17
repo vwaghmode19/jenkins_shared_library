4ee9d7245e40a7eaa986c1cbf1aeedb0fd9029ac
vwaghmode19 <73636631+vwaghmode19@users.noreply.github.com>
2021-09-17 13:27:12 +0530

    Update sample.groovy

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -1,12 +1,13 @@
 class sample {
-	def clone(Script scriptRef, String gitURLString, String branchID, String gitUserID){
-		scriptRef.checkout([
-				$class: 'GitSCM',
-				branches: [[name: branchID]],
-				doGenerateSubmoduleConfigurations: false,
-				extensions: [[$class: 'CleanCheckout']],
-				submoduleCfg: [],
-				userRemoteConfigs: [[credentialsId: gitUserID, url: gitURLString]]
-		])
+	node('master'){
+		stage('Code Checkout'){
+			def clone(Script scriptRef, String gitURLString, String branchID, String gitUserID){
+				scriptRef.checkout([
+					$class: 'GitSCM',
+					branches: [[name: branchID]],
+					userRemoteConfigs: [[credentialsId: gitUserID, url: gitURLString]]
+				])
+			}
+		}
 	}
 }
