2af2ba4cca80e6c1d0c66fcf6b16be47c2b07196
vwaghmode19 <73636631+vwaghmode19@users.noreply.github.com>
2021-09-17 14:10:55 +0530

    Update simpleCheckout.groovy

--- vars/simpleCheckout.groovy
+++ vars/simpleCheckout.groovy
@@ -1,3 +1,7 @@
 def simpleCheckout(){
-checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'vwaghmode19-github', url: 'git@github.com:vwaghmode19/ansible.git']]])
+  checkout([$class: 'GitSCM', 
+            branches: [[name: '*/main']],
+            extensions: [], 
+            userRemoteConfigs: [[credentialsId: 'vwaghmode19-github', url: 'git@github.com:vwaghmode19/ansible.git']]
+           ])
 }
