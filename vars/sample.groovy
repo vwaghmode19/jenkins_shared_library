3f104111e6f9ca27ef08f1a7cc2e9b7355b099a1
vwaghmode19 <73636631+vwaghmode19@users.noreply.github.com>
2021-09-17 13:29:33 +0530

    Update sample.groovy

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -1,11 +1,9 @@
 class sample {
-		stage('Code Checkout'){
-			def clone(Script scriptRef, String gitURLString, String branchID, String gitUserID){
-				scriptRef.checkout([
-					$class: 'GitSCM',
-					branches: [[name: branchID]],
-					userRemoteConfigs: [[credentialsId: gitUserID, url: gitURLString]]
-				])
-			}
-		}
+	def clone(Script scriptRef, String gitURLString, String branchID, String gitUserID){
+		scriptRef.checkout([
+			$class: 'GitSCM',
+			branches: [[name: branchID]],
+			userRemoteConfigs: [[credentialsId: gitUserID, url: gitURLString]]
+		])
+	}
 }
