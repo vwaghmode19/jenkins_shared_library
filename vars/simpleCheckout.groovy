ab3797ecc4cd8d0fd3dce9664fa01455a8c41cf7
vwaghmode19 <73636631+vwaghmode19@users.noreply.github.com>
2021-09-17 14:11:37 +0530

    Delete simpleCheckout.groovy

--- vars/simpleCheckout.groovy
+++
@@ -1,7 +0,0 @@
-def simpleCheckout(){
-  checkout([$class: 'GitSCM', 
-            branches: [[name: '*/main']],
-            extensions: [], 
-            userRemoteConfigs: [[credentialsId: 'vwaghmode19-github', url: 'git@github.com:vwaghmode19/ansible.git']]
-           ])
-}
