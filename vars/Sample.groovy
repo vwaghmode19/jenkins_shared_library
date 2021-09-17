b92b5e1ad2f904c938e68662e3e32de07e28a8b7
vwaghmode19 <73636631+vwaghmode19@users.noreply.github.com>
2021-09-17 13:30:53 +0530

    Update and rename sample.groovy to Sample.groovy

---
+++ vars/Sample.groovy
@@ -0,0 +1,9 @@
+class Sample {
+	def clone(Script scriptRef, String repoURL, String branchName, String gitCredentialsId){
+		scriptRef.checkout([
+			$class: 'GitSCM',
+			branches: [[name: branchName]],
+			userRemoteConfigs: [[credentialsId: gitCredentialsId, url: repoURL]]
+		])
+	}
+}
--- vars/sample.groovy
+++
@@ -1,9 +0,0 @@
-class sample {
-	def clone(Script scriptRef, String gitURLString, String branchID, String gitUserID){
-		scriptRef.checkout([
-			$class: 'GitSCM',
-			branches: [[name: branchID]],
-			userRemoteConfigs: [[credentialsId: gitUserID, url: gitURLString]]
-		])
-	}
-}
