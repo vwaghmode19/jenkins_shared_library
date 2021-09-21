5a16c21846050a5460a78886b4195e0674c011d8
vwaghmode19 <73636631+vwaghmode19@users.noreply.github.com>
2021-09-21 14:57:51 +0530

    Update shared_Methods.groovy

--- vars/shared_Methods.groovy
+++ vars/shared_Methods.groovy
@@ -10,12 +10,10 @@ def sayHello(String name = 'human') {
 def checkout(String node, String branchName, String credentialsId){
         node(node){
                 stage("Checkout") {
-                        script{
                                 checkout([$class           : 'GitSCM',
                                         branches          : [[name: branchName]],
                                         userRemoteConfigs : [[credentialsId: credentialsId, url: 'git@github.com:vwaghmode19/simple-java-maven-app.git']]
                                 ])
-                        }
                 }
         }
 }
