aad905ac099d7d180226f2332f833ad7d943e723
vwaghmode19 <73636631+vwaghmode19@users.noreply.github.com>
2021-09-22 18:19:20 +0530

    Update simpleCheckout.groovy

--- vars/simpleCheckout.groovy
+++ vars/simpleCheckout.groovy
@@ -3,19 +3,13 @@ class SimpleCheckout{
         String repoCredId
         String repoURL
 
-        public static void main(String[] args){
-                println ("Executing getSource Function")
-        }
-
-        def getSource(String branchName, String repoCredId, String repoURL){
+        void getSource(String branchName, String repoCredId, String repoURL){
                 println ("Branch Name:" +branchName +
-                                 "Repo Cred ID:" +repoCredId +
-                                 "Repo URL:" +repoURL)
+                                " \n Repo Cred ID:" +repoCredId +
+                                " \n Repo URL:" +repoURL )
 
-                checkout([$class: 'GitSCM',
-                                   branches: [[name: '*/main']],
-                                   userRemoteConfigs: [[credentialsId: 'vwaghmode19-github', url: 'git@github.com:vwaghmode19/jenkins_shared_library.git']]
-                                ])
-
-        }
+/*              checkout([$class: 'GitSCM',
+                                branches: [[name: 'main']],
+                                userRemoteConfigs: [[credentialsId: 'vwaghmode19-github', url: 'git@github.com:vwaghmode19/jenkins_shared_library.git']]])
+*/      }
 }
