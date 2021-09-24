bb0c32f6079bd60ebf9d109601573472ed8ee5b5
vwaghmode19 <73636631+vwaghmode19@users.noreply.github.com>
2021-09-24 09:31:51 +0530

    Update RepoCheckout.groovy

--- vars/RepoCheckout.groovy
+++ vars/RepoCheckout.groovy
@@ -1,8 +1,8 @@
 def getSource(String branchName, String credsID, String repoURL) {
-  System.out.println("Checking out the code using git module")
-  git branch: "${branchName}", credentialsId: "${credsID}", url: "${repoURL}"
+//   System.out.println("Checking out the code using git module")
+//   git branch: "${branchName}", credentialsId: "${credsID}", url: "${repoURL}"
   
-  System.out.println("Checking out the code using checkout module")
+//   System.out.println("Checking out the code using checkout module")
   checkout([$class: 'GitSCM',
               branches: [[name: 'main']],
               extensions: [],
