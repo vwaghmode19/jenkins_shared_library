e6cb991a7b53a36429c1d65488934849941db4fe
vwaghmode19 <73636631+vwaghmode19@users.noreply.github.com>
2021-09-24 09:18:52 +0530

    Update Zot.groovy

--- vars/Zot.groovy
+++ vars/Zot.groovy
@@ -1,5 +1,4 @@
 def checkOutFrom(String branchName, String credsID, String repoURL) {
-  //git url: "git@github.com:vwaghmode19/${repo}"
   git branch: "${branchName}", credentialsId: "${credsID}", url: "${repoURL}"
 }
 
