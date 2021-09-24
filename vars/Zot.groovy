af90eb2b62fe21f779d5eedeaa4c587a91591f69
vwaghmode19 <73636631+vwaghmode19@users.noreply.github.com>
2021-09-24 09:11:32 +0530

    Update Zot.groovy

--- vars/Zot.groovy
+++ vars/Zot.groovy
@@ -1,6 +1,6 @@
 def checkOutFrom(repo) {
   //git url: "git@github.com:vwaghmode19/${repo}"
-  git branch: 'main', credentialsId: 'vwaghmode19-github', url: 'git@github.com:vwaghmode19/${repo}'
+  git branch: 'main', credentialsId: 'vwaghmode19-github', url: 'git@github.com:vwaghmode19/ansible.git'
 }
 
 return this
