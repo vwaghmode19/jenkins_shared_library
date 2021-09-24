d8c481f73ce31a55db8eb0401132d4a0667928c6
vwaghmode19 <73636631+vwaghmode19@users.noreply.github.com>
2021-09-24 12:39:13 +0530

    Update SharedMethods.groovy

--- vars/SharedMethods.groovy
+++ vars/SharedMethods.groovy
@@ -1,22 +1,23 @@
 #!/usr/bin/env groovy
 def sayHello(String nodeName, String name) {
   node(nodeName){
-        stage("Say Hello"){
-        echo "Hello, ${name}."
-        }
+    stage("Say Hello"){
+      echo "Hello, ${name}."
+    }
   }
 }
 
 //def getSource(String nodeName = 'master', String branchName = 'main', String credsID = 'vwaghmode19-github', String repoURL){
 def getSource(String nodeName, String branchName, String credsID, String repoURL){
-        node(nodeName){
-                stage("Code Checkout") {
-                                checkout scm: [$class     : 'GitSCM',
-                                        branches          : [[name: branchName]],
-                                        userRemoteConfigs : [[credentialsId: credsID, url: repoURL]]
-                                ]
-                }
-        }
+  node(nodeName){
+    stage("Code Checkout") {
+      echo "Checking out ${branchName} branch from ${repoURL} repository"
+      checkout scm: [$class             : 'GitSCM',
+                     branches           : [[name: branchName]],
+                     userRemoteConfigs  : [[credentialsId: credsID, url: repoURL]]
+                    ]
+    }
+  }
 }
 
 return this
