b0e9652abaef1113a3d09d5f1b7ef34859cf0664
vwaghmode19 <73636631+vwaghmode19@users.noreply.github.com>
2021-09-24 08:34:26 +0530

    Update SimpleCheckout.groovy

--- vars/SimpleCheckout.groovy
+++ vars/SimpleCheckout.groovy
@@ -1,10 +1,7 @@
 import jenkins.model.*
 class SimpleCheckout{
       void getSource(String branchName, String repoCredId, String repoURL){
-            echo "Hi There"
-                println ("Branch Name:" +branchName +
-                                " \n Repo Cred ID:" +repoCredId +
-                                " \n Repo URL:" +repoURL )
+            println ("Branch Name:" +branchName + " Repo Cred ID:" +repoCredId +" Repo URL:" +repoURL)
 
 /*              checkout([$class: 'GitSCM',
                                 branches: [[name: 'main']],
