eb28d5e6b06c0cb8bc4c69f45cc2dfa7f867bddb
vwaghmode19 <73636631+vwaghmode19@users.noreply.github.com>
2021-09-24 07:47:58 +0530

    Update TestA.groovy

--- vars/TestA.groovy
+++ vars/TestA.groovy
@@ -1,5 +1,18 @@
 class TestA{
-  void getSource(String root){
-    root.checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'vwaghmode19-github', url: 'git@github.com:vwaghmode19/jenkins_shared_library.git']]])
+  void getSource(){
+    //root.checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'vwaghmode19-github', url: 'git@github.com:vwaghmode19/jenkins_shared_library.git']]])
+    
+    checkout(
+    scm: [
+      $class: 'GitSCM',
+      branches: [[name: "main"]],
+      extensions: scmExtensions,
+      userRemoteConfigs: [[
+        credentialsId: "vwaghmode19-github",
+        name: 'origin',
+        url: "git@github.com:vwaghmode19/ansible.git"
+      ]]
+    ]
+  )
   }
 }
