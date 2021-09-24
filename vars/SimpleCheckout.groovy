63a673011509cd1313d010d3b0afecb3a89e74f1
vwaghmode19 <73636631+vwaghmode19@users.noreply.github.com>
2021-09-24 08:35:46 +0530

    Update SimpleCheckout.groovy

--- vars/SimpleCheckout.groovy
+++ vars/SimpleCheckout.groovy
@@ -2,9 +2,8 @@ import jenkins.model.*
 class SimpleCheckout{
       void getSource(String branchName, String repoCredId, String repoURL){
             System.out.println ("Branch Name:" +branchName + " Repo Cred ID:" +repoCredId +" Repo URL:" +repoURL)
-
-/*              checkout([$class: 'GitSCM',
+            checkout([$class: 'GitSCM',
                                 branches: [[name: 'main']],
                                 userRemoteConfigs: [[credentialsId: 'vwaghmode19-github', url: 'git@github.com:vwaghmode19/jenkins_shared_library.git']]])
-*/      }
+      }
 }
