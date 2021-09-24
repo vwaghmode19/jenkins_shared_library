2294cd2c12c8ce7998d921f1c4f2ef10ee5c5b43
vwaghmode19 <73636631+vwaghmode19@users.noreply.github.com>
2021-09-24 09:30:09 +0530

    Update RepoCheckout.groovy

--- vars/RepoCheckout.groovy
+++ vars/RepoCheckout.groovy
@@ -1,5 +1,12 @@
 def getSource(String branchName, String credsID, String repoURL) {
+  System.out.println("Checking out the code using git module")
   git branch: "${branchName}", credentialsId: "${credsID}", url: "${repoURL}"
+  
+  System.out.println("Checking out the code using checkout module")
+  checkout([$class: 'GitSCM',
+              branches: [[name: 'main']],
+              extensions: [],
+              userRemoteConfigs: [[credentialsId: 'vwaghmode19-github', url: 'git@github.com:vwaghmode19/jenkins_shared_library.git']]])
 }
 
 return this
