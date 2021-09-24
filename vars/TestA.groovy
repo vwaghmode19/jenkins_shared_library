b4e170233b41a6668b14f5c180573cce75b640be
vwaghmode19 <73636631+vwaghmode19@users.noreply.github.com>
2021-09-24 07:23:05 +0530

    Update TestA.groovy

--- vars/TestA.groovy
+++ vars/TestA.groovy
@@ -1,5 +1,5 @@
 class TestA{
-  void getSource(Script root){
+  void getSource(String root){
     root.checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'vwaghmode19-github', url: 'git@github.com:vwaghmode19/jenkins_shared_library.git']]])
   }
 }
